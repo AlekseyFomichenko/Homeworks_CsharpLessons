ce13a5e658bc1c58891d0e4fd92c2172e1e6a984
Aleksey <fomichaaf.95@gmail.com>
2023-04-15 22:05:06 +0300

    навёл красоту в коде

--- HomeworkSeminar3.cs
+++ HomeworkSeminar3.cs
@@ -1,11 +1,7 @@
 // Задача 19
-
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
 // 14212 -> нет
-
 // 12821 -> да
-
 // 23432 -> да 
 
 // bool IsPalindrome(int arg)
@@ -27,15 +23,13 @@
 // bool result = IsPalindrome(input);
 // Console.WriteLine(result);
 
+
 // Задача 21
-
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-
 // A (3,6,8); B (2,1,-7), -> 15.84
-
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// static double SegmentDistance(double[] a, double[] b)
+// double SegmentDistance(double[] a, double[] b)
 // {
 //     return Math.Round(Math.Sqrt((b[0] - a[0]) * (b[0] - a[0]) +
 //                                 (b[1] - a[1]) * (b[1] - a[1]) +
@@ -49,21 +43,19 @@
 // Console.WriteLine(lineAB);
 
 // Задача 23
-
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-static void TableOfCubes(int n)
-{
-    int current = 1;
-    while (current <= n)
-    {
-        Console.WriteLine($"{current}\t{Math.Pow(current, 3)}\n");
-        current++;
-    }
-}
-Console.Write("Введите число: ");
-int userInput = Convert.ToInt32(Console.ReadLine());
-TableOfCubes(userInput);
+// void TableOfCubes(int n)
+// {
+//     int current = 1;
+//     while (current <= n)
+//     {
+//         Console.WriteLine($"{current}\t{Math.Pow(current, 3)}\n");
+//         current++;
+//     }
+// }
+// Console.Write("Введите число: ");
+// int userInput = Convert.ToInt32(Console.ReadLine());
+// TableOfCubes(userInput);
